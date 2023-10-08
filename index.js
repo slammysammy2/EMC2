3f4363251da7a9b6869734635224205f387c32b2
slammysammy2 <113303157+slammysammy2@users.noreply.github.com>
2023-10-08 18:05:30 +0200

    animated scroll attempt 3

--- index.js
+++ index.js
@@ -2,20 +2,21 @@ function openNewPage() {
     window.open("https://calendly.com/?utm_source=google&utm_medium=cpc&utm_campaign=Sign-Ups_Intl-Brand_EMEA-English_Search&utm_adgroup=calendly-exact-EMEA-English&utm_content=calendly-exact-EMEA-English&utm_term=calendly&utm_matchtype=e&utm_targetid=kwd-309663638777&utm_location=1000997&utm_placement=&utm_device=c&gad=1&gclid=CjwKCAjwseSoBhBXEiwA9iZtxgBJHwXvFIqSBJrdPcNVRELJIYdjlGInx2ryqBnjwtK7l0KF-k5hcRoCHsYQAvD_BwE", "_blank");
 }
 
-function section() {
-    var sections = document.querySelectorAll(".reveal");
-    for (var i = 0; i < sections.length; i++) {
+function reveal() {
+    var reveals = document.querySelectorAll(".reveal");
+  
+    for (var i = 0; i < reveals.length; i++) {
       var windowHeight = window.innerHeight;
-      var elementTop = sections[i].getBoundingClientRect().top;
+      var elementTop = reveals[i].getBoundingClientRect().top;
       var elementVisible = 150;
+  
       if (elementTop < windowHeight - elementVisible) {
-        sections[i].classList.add("active");
+        reveals[i].classList.add("active");
       } else {
-        sections[i].classList.remove("active");
+        reveals[i].classList.remove("active");
       }
     }
   }
-
+  
   window.addEventListener("scroll", reveal);
-
-
+  
