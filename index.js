d984a5a420742c7bb23e8aa3300e3b55457bcd74
slammysammy2 <113303157+slammysammy2@users.noreply.github.com>
2023-10-22 21:36:16 +0200

    added coaching text

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ function reveal() {
     for (var i = 0; i < reveals.length; i++) {
       var windowHeight = window.innerHeight;
       var elementTop = reveals[i].getBoundingClientRect().top;
-      var elementVisible = 150;
+      var elementVisible = 100;
   
       if (elementTop < windowHeight - elementVisible) {
         reveals[i].classList.add("active");
@@ -19,41 +19,3 @@ function reveal() {
   }
   
   window.addEventListener("scroll", reveal);
-
-
-
-  console.clear();
-
-  gsap.registerPlugin(ScrollTrigger);
-  
-  let pinGroups = gsap.utils.toArray(".pin-group");
-  pinGroupContent = gsap.utils.toArray(".pin-group > *");
-  
-  pinGroups.forEach((pingroup) => {
-    ScrollTrigger.create({
-      trigger: pingroup,
-      start: "center center",
-      end: "+=" + window.innerHeight / 2,
-      pin: true
-    });
-  });
-  
-  gsap.set(pinGroupContent, {
-    opacity: 0,
-    y: 100
-  });
-  
-  const enterConfig = { y: 0, opacity: 1, duration: 0.4 };
-  
-  pinGroupContent.forEach((e, i) => {
-    ScrollTrigger.create({
-      trigger: e,
-      start: "center 80%",
-      end: "bottom 40%",
-      onEnter: () => gsap.to(e, enterConfig),
-      onEnterBack: () => gsap.to(e, enterConfig),
-      onLeave: () => gsap.to(e, { y: -100, opacity: 0, duration: 0.4 }),
-      onLeaveBack: () => gsap.to(e, { y: 100, opacity: 0, duration: 0.4 }),
-      markers: true
-    });
-  });
