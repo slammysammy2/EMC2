dec29b5dc895e459a2bb9efcd2c1e9837fa0aa88
slammysammy2 <113303157+slammysammy2@users.noreply.github.com>
2023-10-23 14:23:58 +0200

    deleted line 16

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ function reveal() {
       if (elementTop < windowHeight - elementVisible) {
         reveals[i].classList.add("active");
       } else {
-        reveals[i].classList.remove("active");
+     
       }
     }
   }
