ade2b54a9ac593716bfdb4eab6872797bc8dd338
slammysammy2 <113303157+slammysammy2@users.noreply.github.com>
2024-05-30 20:01:58 +0200

    t

--- index.js
+++ index.js
@@ -67,18 +67,28 @@ form.addEventListener("submit", function (e) {
     });
 });
 
-document.addEventListener('DOMContentLoaded', () => {
-  const carouselInner = document.querySelector('.carousel-inner');
-  const carouselItems = document.querySelectorAll('.carousel-item');
-  let currentIndex = 0;
-
-  function showNextItem() {
-      carouselItems[currentIndex].classList.remove('active');
-      currentIndex = (currentIndex + 1) % carouselItems.length;
-      carouselItems[currentIndex].classList.add('active');
-      carouselInner.style.transform = `translateX(-${currentIndex * 100}%)`;
-  }
-
-  setInterval(showNextItem, 3000); // Change image every 3 seconds
-});
+window.onload = function(){
+  var slides = document.getElementsByClassName('carousel-item'),
+      addActive = function(slide) {slide.classList.add('active')},
+      removeActive = function(slide) {slide.classList.remove('active')};
+  addActive(slides[0]);
+  
+  setInterval(function (){
+    for (var i = 0; i < slides.length; i++){
+      if (i + 1 == slides.length) {
+        addActive(slides[0]);
+        slides[0].style.zIndex = 100;
+        setTimeout(removeActive, 350, slides[i]); //Doesn't be worked in IE-9
+        break;
+      }
+      if (slides[i].classList.contains('active')) { 
+        slides[i].removeAttribute('style');
+        setTimeout(removeActive, 350, slides[i]); //Doesn't be worked in IE-9
+        addActive(slides[i + 1]);
+        break;
+      }
+    } 
+  }, 4000);
+}
+  
 
