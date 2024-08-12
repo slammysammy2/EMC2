edf01a2e59a148e86573fd28bf2d5f5f2d67cb23
slammysammy2 <113303157+slammysammy2@users.noreply.github.com>
2024-08-12 17:17:44 +0200

    added animation

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+/* slider  */
+
 const scrollers = document.querySelectorAll(".scroller");
 
 if(!window.matchMedia("(prefers-reduced-motion:reduce)").matches) {
@@ -9,6 +11,8 @@ function addAnimation() {
     scroller.setAttribute("data-animated", true);
   });
 }
+
+/* slider  */
 
 function openNewPage() {
     window.open("https://calendly.com/?utm_source=google&utm_medium=cpc&utm_campaign=Sign-Ups_Intl-Brand_EMEA-English_Search&utm_adgroup=calendly-exact-EMEA-English&utm_content=calendly-exact-EMEA-English&utm_term=calendly&utm_matchtype=e&utm_targetid=kwd-309663638777&utm_location=1000997&utm_placement=&utm_device=c&gad=1&gclid=CjwKCAjwseSoBhBXEiwA9iZtxgBJHwXvFIqSBJrdPcNVRELJIYdjlGInx2ryqBnjwtK7l0KF-k5hcRoCHsYQAvD_BwE", "_blank");
