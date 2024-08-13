805aafec33ae056eb1cb082c439809102c18ce44
slammysammy2 <113303157+slammysammy2@users.noreply.github.com>
2024-08-13 13:43:51 +0200

    swtiched testimonial and carousel

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const scrollers = document.querySelectorAll(".scroller");
 
-if(window.matchMedia("(prefers-reduced-motion: reduce)").matches) {
+if(!window.matchMedia("(prefers-reduced-motion: reduce)").matches) {
   addAnimation();
 }
 
@@ -8,7 +8,7 @@ function addAnimation() {
   scrollers.forEach((scroller) => {
     scroller.setAttribute("data-animated", true);
 
-    const scrollerInner = scroller.querySelector('.scroller_inner');
+    const scrollerInner = scroller.querySelector('.scroller__inner');
     const scrollerContent = Array.from(scrollerInner.children);
 
     scrollerContent.forEach((item) => {
