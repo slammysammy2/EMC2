c8cd62895785a7514575de9cf6e66631799de378
slammysammy2 <113303157+slammysammy2@users.noreply.github.com>
2024-08-13 14:26:45 +0200

    test

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const scrollers = document.querySelectorAll(".scroller");
 
-if(!window.matchMedia("(prefers-reduced-motion: reduce)").matches) {
+if (!window.matchMedia("(prefers-reduced-motion: reduce)").matches) {
   addAnimation();
 }
 
